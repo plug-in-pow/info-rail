031e5f3342f9072915f261774355c2f3f5007b42
plug-in-pow <tripathimayank36@gmail.com>
2022-11-11 15:10:33 +0530

    added list-tile view and added barcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as JsBarcode from 'jsbarcode';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'info-rail';
+
+  ngOnInit() {
+    JsBarcode('#barcode', '12345', {
+      height: 50,
+      width: 2.3,
+      text: '04601'
+    });
+  }
 }
